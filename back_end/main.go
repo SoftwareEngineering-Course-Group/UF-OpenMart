7523f965b4b54b4361bbe44b89ce5dcb635962f3
huangqirui <huangqirui@ufl.edu>
2022-02-01 14:44:17 -0500

    using gorm to create tables in sqlites

--- back_end/main.go
+++ back_end/main.go
@@ -1,1 +1,29 @@
-package back_end
+package main
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID uint
+}
+type Item struct {
+	ID uint
+}
+type Comment struct {
+	ID uint
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("sqlite.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Item{})
+	db.AutoMigrate(&Comment{})
+
+}
