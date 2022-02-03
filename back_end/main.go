86c6a8a9b3e7beb0645aff3375cf40cd9bcd1c01
huangqirui <pauldhuang@hotmail.com>
2022-02-03 13:17:04 -0500

    make email unique

--- back_end/main.go
+++ back_end/main.go
@@ -17,7 +17,7 @@ type User struct {
 	ID            uint   `gorm:"primaryKey,autoIncrement"`
 	Name          string `gorm:"not null,unique"`
 	Password      string `gorm:"not null"`
-	Email         string `gorm:"not null"`
+	Email         string `gorm:"not null,unique"`
 	Phone         string `gorm:"not null"`
 	nonce         string
 	publicAddress string
