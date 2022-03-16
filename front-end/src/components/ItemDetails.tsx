d4e4951aff68d8b92e0783c47a092575f034c8ce
7thDecember <2685642987@qq.com>
2022-03-16 15:21:22 -0400

    collection icon

--- front-end/src/components/ItemDetails.tsx
+++ front-end/src/components/ItemDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Image,Card,Icon } from 'semantic-ui-react'
 
 const item = 
@@ -8,12 +8,25 @@ const item =
       image:'../images/2cc3723492e356375e6e26cacc12407.jpg',
       price: 3
     }
-const ItemDetails = () => (
+
+
+const ItemDetails = () => {
+    const [love, setlove] = useState(false);
+    const handleClick=() =>{
+        setlove(!love);
+    }
+    var loveIcon:any;
+    if(love){
+        loveIcon=(<Icon name='heart' size='large' color='red' link style={{float:'right'}}/>);
+    }else{
+        loveIcon=(<Icon name='heart outline' size='large' color='red' link style={{float:'right'}}/>);
+    }
+    return(
     <div>
         <Card style={{width:'100%'}}>
             <Image src={item.image} fluid />
             <Card.Content >
-                <Icon name='heart outline' size='large' color='red' link style={{float:'right'}}/>
+                <div onClick={() => handleClick()}>{loveIcon}</div>
                 <Card.Header>{item.name}</Card.Header>
                 <Card.Header>{item.price}$</Card.Header>
                 <Card.Meta>
@@ -25,6 +38,6 @@ const ItemDetails = () => (
             </Card.Content>
         </Card>
     </div>
-)
+)}
 
 export default ItemDetails
