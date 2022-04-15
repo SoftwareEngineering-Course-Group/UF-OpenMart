d31162deb7f0c3fad24813735425dab9e5f4b932
Yuyangbo <976644121@qq.com>
2022-04-15 14:22:39 -0400

    item name fix and require login

--- front-end/src/pages/Item.tsx
+++ front-end/src/pages/Item.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from '../components/Menu'
 import Back from '../components/Back'
 import Comments from '../components/Comments'
 import ItemHeader from '../components/ItemHeader'
 import ItemDetails from '../components/ItemDetails';
-import { useLocation,useParams } from 'react-router-dom';
+import { useLocation,useNavigate,useParams } from 'react-router-dom';
 import { getItembyId, getName } from '../utils';
+import { Button, Modal } from 'semantic-ui-react';
 
 const SERVER_ORIGIN = "http://localhost:12345";
 function Item() {
@@ -21,8 +22,9 @@ function Item() {
     const [title, setTitle] = React.useState("")
     const [price, setPrice] = React.useState("")
     const [des, setDes] = React.useState("")
-    
+    const [open, setOpen] = useState(false)
     var isMine=false;
+    const navigate = useNavigate();
     useEffect( ()=>{
         console.log(para)
         getItembyId(para.id).then((res: any) =>{
@@ -30,14 +32,34 @@ function Item() {
             setTitle(res.Name);
             setPrice(res.Price);
             setDes(res.Description);
+            setOpen(false)
             console.log(img);
         }).catch((err) => {
             console.log(err)
+            setOpen(true)
             console.log("err in getItems")
         })       
     })
     return (
         <div>
+            <Modal
+                centered={false}
+                open={open}
+                // onClose={() => setOpen(false)}
+                // onOpen={() => setOpen(true)}
+                // trigger={<Button>Show Modal</Button>}
+                >
+                <Modal.Header>Failed to access</Modal.Header>
+                <Modal.Content>
+                    <Modal.Description>
+                    Please login first...
+                    </Modal.Description>
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button onClick={() => navigate("/login")}>Back to Login</Button>
+                </Modal.Actions>
+            </Modal>
+
             <Back/>
             <ItemHeader userId={sta.userid} itemId={para.id} profile={isMine}/>
             <div>
